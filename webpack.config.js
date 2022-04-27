a808fa53e9269782ef13aac2684405b7a56656ef
Sudarshan <sudharshan@tagalys.com>
2022-04-27 22:58:25 +0530

    misc

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const CompressionPlugin = require("compression-webpack-plugin");
+// const path = require('path');
+// const CompressionPlugin = require("compression-webpack-plugin");
 
-module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: './src/index.ts',
-  plugins: [new CompressionPlugin()],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 9000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-    library: 'Tagalys',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    },
-    port: 9002
-  }
-};
+// module.exports = {
+//   mode: process.env.NODE_ENV,
+//   entry: './src/index.ts',
+//   plugins: [new CompressionPlugin()],
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'public'),
+//     },
+//     compress: true,
+//     port: 9000,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/,
+//       }
+//     ],
+//   },
+//   output: {
+//     filename: 'bundle.js',
+//     library: 'Tagalys',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   resolve: {
+//     extensions: [ '.tsx', '.ts', '.js' ],
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist')
+//     },
+//     port: 9002
+//   }
+// };
