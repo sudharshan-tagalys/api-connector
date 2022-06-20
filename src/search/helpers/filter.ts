5037237a283645958752e0a693dcdff66ed3d33c
Kiruba Karthikeyan <kiruba@tagalys.com>
2022-06-20 21:02:22 +0530

    added filter helpers

--- src/search/helpers/filter.ts
+++ src/search/helpers/filter.ts
@@ -3,29 +3,72 @@ const getFilters = function(){
 }
 
 const getAppliedFilters = function(){
-
+  return getAppliedFilterItems(this.responseState.filters)
 }
 
-const applyFilter = function(){
-
-}
-const getFilterById = function(){
-
-}
-const getAppliedFilterById = function(){
-
-}
-
-const isFilterApplied = function(){
-
+const flattenFilterItems = function(items){
+  let appliedFilterItems = []
+  items.forEach((item)=>{
+    if(item.hasOwnProperty('items')){
+      appliedFilterItems.concat(flattenFilterItems(item.items))
+    }else{
+      appliedFilterItems.push(item);
+    }
+  })
+  return appliedFilterItems
 }
 
-const clearFilter = function(){
+const getAppliedFilterItems = function(items){
+  const flattenedFilterItems = flattenFilterItems(items);
+  return flattenedFilterItems.filter((filter)=>filter.selected)
+}
 
+const applyFilter = function(filterId, filterItemsToApply){
+  this.setRequestState((reqState)=>{
+    let filter = (reqState.filters[filterId] || [])
+    if(Array.isArray(filterItemsToApply)){
+      filter = filter.concat(filterItemsToApply)
+    }else{
+      filter = filter.push(filterItemsToApply)
+    }
+    reqState.filters[filterId] = filter;
+    return reqState
+  })
+}
+const getFilterById = function(filterId){
+  const flattenedFilterItems = flattenFilterItems(this.responseState.filters);
+  return flattenedFilterItems.find((filter)=> filter.id === filterId)
+}
+
+const getAppliedFilterById = function(filterId){
+  const appliedFilters = getAppliedFilters.call(this)
+  return appliedFilters.find((filter)=>filter.id === filterId)
+}
+
+const isFilterApplied = function(filterId){
+  const appliedFilter = getAppliedFilterById.call(this)
+  if(appliedFilter && Object.keys(appliedFilter).length){
+    return true
+  }
+  return false
+}
+
+const clearFilter = function(filterId, filterItemIds = []){
+  this.setRequestState((reqState)=>{
+    if(filterItemIds.length === 0){
+      delete reqState.filters[filterId]
+    }else{
+      reqState.filters[filterId] = reqState.filters[filterId].filter((filterItemId)=>filterItemIds.includes(filterItemId))
+    }
+    return reqState
+  })
 }
 
 const clearAllFilters = function(){
-
+  this.setRequestState((reqState)=>{
+    reqState.filters = {}
+    return reqState
+  })
 } 
 
 export default {
