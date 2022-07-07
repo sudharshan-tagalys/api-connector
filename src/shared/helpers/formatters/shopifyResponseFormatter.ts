8b233d449cf84150d8eb60f44db36f8177c893f8
Kiruba Karthikeyan <kiruba@tagalys.com>
2022-07-07 13:41:12 +0530

    added shopify platfom fields

--- src/shared/helpers/formatters/shopifyResponseFormatter.ts
+++ src/shared/helpers/formatters/shopifyResponseFormatter.ts
@@ -1,6 +1,30 @@
 import Formatter from './formatter';
 import SuggestionsFormatter from "../../../search-suggestions/suggestionsFormatter"
 
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index
+}
+
+// has_only_default_variant
+// compare_at_price_varies
+// price_varies
+// price_min
+// compare_at_price_min
+// options
+// -- options_with_values
+
+const hasOnlyDefaultVariant = (detail) => {
+  if(detail.options.length > 1){
+    return false
+  }
+  if(detail.variants.length > 1){
+    return false
+  }
+  if(detail.options[0] === 'Title' && detail.variants[0].option1 === 'Default Title'  && detail.variants[0].option2 === null && detail.variants[0].option3 === null){
+    return true
+  }
+  return false
+}
 class ShopifyResponseFormatter extends Formatter {
   platformFieldTranslations(){
     return {
@@ -44,8 +68,16 @@ class ShopifyResponseFormatter extends Formatter {
   }
 
   additionalPlatformFields(detail){
+    const variantCompareAtPrices = detail.variants.map(variant => variant.compare_at_price)
+    const variantPrices = detail.variants.map(variant => variant.price)
     return {
-      handle: detail.link.split("/products/")[1]
+      handle: detail.link.split("/products/")[1],
+      compare_at_price_min: detail.price,
+      price_min: detail.sale_price,
+      options: detail.options,
+      compare_at_price_varies: variantCompareAtPrices.filter(unique).length > 1,
+      price_varies: variantPrices.filter(unique).length > 1,
+      has_only_default_variant: hasOnlyDefaultVariant(detail)
     }
   }
 
