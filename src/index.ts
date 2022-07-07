0ec5135c8029dcb697d4422b330e66593fcb293e
Kiruba Karthikeyan <kiruba@tagalys.com>
2022-07-08 05:15:35 +0530

    track on set configuration

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ import AddedToCartAlsoAddedToCart from "./added-to-cart-also-added-to-cart"
 import SearchSuggestions from "./search-suggestions"
 import Search from './search'
 import queryStringManager from "./lib/queryStringManager";
+import ShopifyAnalyticsTracker from './lib/shopifyAnalyticsTracker'
 import { getRequestParamsFromQueryString } from "./shared/helpers/common";
+import cookie from "./lib/cookie";
+import { COOKIES } from "./lib/analyticsTracker";
 
 export const APIConnector = {
   ...Search.export(),
@@ -18,10 +21,19 @@ export const APIConnector = {
   ...ViewedAlsoViewed.export(),
   ...AddedToCartAlsoAddedToCart.export(),
   ...SearchSuggestions.export(),
-  setConfiguration: (config) => configuration.setConfiguration({
-    ...DEFAULT_CONFIGURATION,
-    ...config
-  }),
+  setConfiguration: (config) => {
+    configuration.setConfiguration({
+      ...DEFAULT_CONFIGURATION,
+      ...config
+    })
+    const canTrackAnalytics = (config.track && config.analyticsStorageConsentProvided())
+    if(config.platform === 'Shopify' && canTrackAnalytics){
+      const shopifyAnalyticsTracker = new ShopifyAnalyticsTracker()
+      shopifyAnalyticsTracker.track()
+    }else{
+      cookie.batchDelete(Object.values(COOKIES))
+    }
+  },
 setQueryStringConfiguration: (config) => queryStringManager.setConfiguration(config),
   getRequestParamsFromQueryString: (queryString) => getRequestParamsFromQueryString(queryString)
 }
