c3cbaf80dce78e9dc350fdff8ae90bce6d3e7a65
Kiruba Karthikeyan <kiruba@tagalys.com>
2022-07-08 16:15:46 +0530

    added dist files

--- dist/lib/shopifyAnalyticsTracker.d.ts
+++ dist/lib/shopifyAnalyticsTracker.d.ts
@@ -1,13 +1,11 @@
 declare class ShopifyAnalyticsTracker {
     window(): any;
     getShopifyObject(): any;
-    getCheckoutObject(): any;
     getPageMetaData(): any;
     logTrack(event: any, data: any): void;
     trackProductIfExist(): void;
     trackCollectionIfExist(): void;
     trackCartTokenIfExist(): void;
-    trackOrderIfExist(): void;
     track(): void;
 }
 export default ShopifyAnalyticsTracker;
