ef0212cfede8483cc0aa407880a61ee4cf0b2038
Sudarshan <sudharshan@tagalys.com>
2023-01-23 20:11:26 +0530

    changed platform helpers version

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -258,7 +258,7 @@ function loadTagalysHelperScript() {
   if(_window.TagalysPlatformHelpers) return
   return new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = "https://storage.googleapis.com/tagalys-front-end-components/tagalys-platform-helpers-2023-01-23.js";
+    script.src = "https://storage.googleapis.com/tagalys-front-end-components/tagalys-platform-helpers-v1.0.0.js";
     script.onload = resolve;
     script.onerror = reject;
     document.body.appendChild(script);
