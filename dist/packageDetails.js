4d8c894f85c6c5ec65b4ddb71fdb276f76efd8e6
Sudarshan <sudharshan@tagalys.com>
2023-05-20 13:08:56 +0530

    added dist files

--- dist/packageDetails.js
+++ dist/packageDetails.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var packageDetails = {
     vendor: "tagalys-api-connector",
     language: "js",
-    version: "1.3.2",
+    version: "1.3.3",
 };
 exports.default = packageDetails;
 //# sourceMappingURL=packageDetails.js.map
