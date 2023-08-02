343abe9d9ffe29d3949abdf264818677b49d72cd
Kiruba Karthikeyan <kiruba@tagalys.com>
2023-08-02 16:13:27 +0530

    query-constructor which constructs grapqhl query from request state

---
+++ src/product-lisiting-page/platform/shopify/helpers/graphql-query-constructor.ts
@@ -0,0 +1,277 @@
+import configuration from '../../../../lib/configuration';
+
+class GraphqlQueryConstructor {
+
+  private requestState
+  private responseState
+  
+  constructor(requestState, responseState) {
+    this.requestState = requestState;
+    this.responseState = responseState
+  }
+
+  getSortVariables(){
+    const sortOptionToSortKeyMap = {
+      'manual': {
+        sortKey: "MANUAL",
+        reverse: false
+      },
+      'newest-oldest': {
+        sortKey: 'CREATED',
+        reverse: true
+      },
+      'price-desc': {
+        sortKey: 'PRICE',
+        reverse: true,
+      },
+      'price-asc': {
+        sortKey: 'PRICE',
+        reverse: false 
+      }
+    }
+    return sortOptionToSortKeyMap[this.requestState.sort]
+  }
+
+  getPaginationVariables(){
+    let paginationVariables = {}
+    if(this.requestState.endCursor){
+      paginationVariables['after'] = this.requestState.endCursor
+      paginationVariables['first'] = this.requestState.perPage
+    }
+    if (this.requestState.startCursor) {
+      paginationVariables['before'] = this.requestState.startCursor
+      paginationVariables['last'] = this.requestState.perPage
+    }
+    if (!this.requestState.startCursor && !this.requestState.endCursor) {
+      paginationVariables['first'] = this.requestState.perPage
+    }
+    return paginationVariables
+  }
+
+  getFilterVariables(){
+    let filterVariables = {}
+    if (Object.keys(this.requestState.filters).length) {
+      let filtersToApply = []
+      for (const [_, filterValues] of Object.entries(this.requestState.filters)) {
+        const values: any = filterValues
+        if(filterValues.hasOwnProperty("selected_min")){
+          filtersToApply.push({
+            price: {
+              min: filterValues['selected_min'],
+              max: filterValues['selected_max']
+            }
+          })
+        }else{
+          values.forEach((filterValue) => {
+            const selectedFilterValue = this.responseState.filterInputs[filterValue]
+            filtersToApply.push(JSON.parse(selectedFilterValue.input))
+          })
+        }
+      }
+      filterVariables['filters'] = filtersToApply
+    }
+    return filterVariables
+  }
+
+  getQueryVariables(){
+    return {
+      id: `gid://shopify/Collection/${this.requestState.product_listing_page_id}`,
+      ...this.getSortVariables(),
+      ...this.getPaginationVariables(),
+      ...this.getFilterVariables()
+    }
+  }
+
+  getQuery() {
+    return `
+      query Collection(
+        $id: ID,
+        $first: Int,
+        $last: Int,
+        $before: String,
+        $after: String,
+        $sortKey: ProductCollectionSortKeys,
+        $reverse: Boolean,
+        $filters: [ProductFilter!]
+      ) @inContext(country: ${configuration.getCountryCode()}) {
+        collection(id: $id){
+          title
+          handle
+          products(first: $first, last: $last, after: $after, before: $before, sortKey: $sortKey, reverse: $reverse, filters: $filters) {
+            filters {
+              id
+              label
+              type
+              values {
+                id
+                label
+                count
+                input
+              }
+            }
+            edges{
+              node{
+                ${this.getBasicDetails()}
+                ${this.getVariants()}
+                ${this.getImages()}
+                ${this.getMedia()}
+              }
+            }
+            pageInfo{
+              hasNextPage
+              hasPreviousPage
+              endCursor
+              startCursor
+            }
+          }
+        }
+      }
+    `
+  }
+
+  getImages() {
+    return `
+      images(first: 250, sortKey: POSITION){
+        edges{
+          node{
+            altText
+            height
+            url
+            width
+            id
+          }
+        }
+      }
+    `
+  }
+
+
+  getBasicDetails() {
+    return `
+      id
+      title
+      handle
+      productType
+      vendor
+      tags
+      totalInventory
+      publishedAt
+      availableForSale
+      options{
+        id
+        name
+        values
+      }
+      featuredImage{
+        altText
+        height
+        url
+        width
+        id
+      }
+      priceRange{
+        minVariantPrice{
+          amount
+        }
+        maxVariantPrice{
+          amount
+        }
+      }
+      compareAtPriceRange{
+        minVariantPrice{
+          amount
+        }
+        maxVariantPrice{
+          amount
+        }
+      }
+      featuredImage{
+        altText
+        height
+        url
+        width
+        id
+      }
+    `
+  }
+
+  getMedia() {
+    return `
+      media(first: 250){
+        edges{
+          node{
+            alt
+            mediaContentType
+            ... on Video {
+              id
+              mediaContentType
+              alt
+              sources {
+                format
+                height
+                mimeType
+                width
+                url
+              }
+            }
+            ... on MediaImage{
+              id
+              mediaContentType
+              image{
+                id
+                altText
+                width
+                height
+                url
+              }
+            }
+            ... on ExternalVideo{
+              id
+              mediaContentType
+            }
+            ... on Model3d{
+              id
+              mediaContentType
+            }
+          }
+        }
+      }
+    `
+  }
+
+  getVariants() {
+    return `
+      variants(first: 250){
+        edges{
+          node{
+            id
+            title
+            quantityAvailable
+            sku
+            selectedOptions{
+              name
+              value
+            }
+            image{
+              id
+            }
+            availableForSale
+            price {
+              amount
+            }
+            compareAtPrice{
+              amount
+            }      
+          }
+        }
+      }
+    `
+  }
+
+  getMetafields() {
+    return `
+    `
+  }
+}
+
+export default GraphqlQueryConstructor
