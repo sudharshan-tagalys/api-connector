2208abb4245e69632dff80f4b0fe7eef0b390440
Sudarshan <sudharshan@tagalys.com>
2024-02-25 16:16:13 +0530

    changed package details to 2.0.0-beta3 and added dist files

--- dist/packageDetails.js
+++ dist/packageDetails.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var packageDetails = {
     vendor: "tagalys-api-connector",
     language: "js",
-    version: "2.0.0-beta2",
+    version: "2.0.0-beta3",
 };
 exports.default = packageDetails;
 //# sourceMappingURL=packageDetails.js.map
